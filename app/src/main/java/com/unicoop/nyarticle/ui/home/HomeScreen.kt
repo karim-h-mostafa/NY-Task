50338f666005903b01f224e6151d69b4120ab587
karim <kimo7amed@gmail.com>
2024-12-31 07:50:51 +0200

    add-clean-arch-modules

---
+++ app/src/main/java/com/unicoop/nyarticle/ui/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.unicoop.nyarticle.ui.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
